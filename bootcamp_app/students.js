e940564255ea1841b1eaf5b7046d81b67948b33c
Robert Shum <robertshum@gmail.com>
2023-10-18 11:19:23 -0700

    protected teachers and students from SQL injections!

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -12,20 +12,37 @@ const pool = new Pool({
 //[1] = 2, number of results
 const args = process.argv.slice(2);
 if (args.length == 0) {
+  console.log("please provide at least 1 input");
   return;
 }
 
+const cohortName = args[0];
+const limit = args[1] || 5;
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 // start querying
 pool.query(`
+
 SELECT students.id, students.name as student_name, cohorts.name as cohort_name
 FROM students JOIN cohorts on students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT '${args[1] || 5}';
-`).then(res => {
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+
+`, values).then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student_name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
   });
-}).catch(err => console.error('query error', err.stack));
 
-//end the connection(s)
-pool.end();
+  //end the connection(s)
+  pool.end();
+
+}).catch(err => {
+  console.error('query error', err.stack);
+
+  //end the connection(s)
+  pool.end();
+}
+);
+
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,10 +9,17 @@ const pool = new Pool({
 
 //input from user
 //[0] = "FEB", cohort name
+//[1] = 2, number of results
 const args = process.argv.slice(2);
 if (args.length == 0) {
   return;
 }
+
+const cohortName = args[0];
+const limit = args[1] || 5;
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 // start querying
 pool.query(`
@@ -23,14 +30,22 @@ FROM teachers
   join assistance_requests on teachers.id = assistance_requests.teacher_id
   join students on assistance_requests.student_id = students.id
   join cohorts on students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0]}%'
-ORDER BY teacher;
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+LIMIT $2;
 
-`).then(res => {
+`, values).then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} : ${user.teacher}`);
   });
-}).catch(err => console.error('query error', err.stack));
 
-//end the connection(s)
-pool.end();;
+  //end the connection(s)
+  pool.end();
+
+}).catch(err => {
+
+  //end the connection(s)
+  pool.end();
+
+  console.error('query error', err.stack);
+});
